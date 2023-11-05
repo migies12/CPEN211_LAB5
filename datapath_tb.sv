bd8762bd9bbd470424be8e5845057269b288cba3
SarahMacGillivray <smacg04@gmail.com>
2023-11-05 15:57:13 -0800

    adding sof and compiling files

--- datapath_tb.sv
+++ datapath_tb.sv
@@ -113,7 +113,7 @@ $display("Test #1: Adding 2 and 7 left shifted by 1 (14) which outputs 10000 usi
 
 	$display("Read R3 Output is %b, we expected %b", sim_datapath_out, 16'd16);
 	
-	if(~(sim_datapath_out == 16'd16)) err = 1;
+	if(~(sim_datapath_out == 16'd16)|| sim_Z_out == 1) err = 1;
 	
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //TEST #2: Subtracting 13 and 4 right shifted by 1 (14) which outputs 1011 in R2 and R0
@@ -185,7 +185,7 @@ $display("TEST #2: Subtracting 13 and 4 right shifted by 1 (14) which outputs 10
 
 	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, 16'd11);
 
-	if((sim_datapath_out != 16'd11)) err = 1;
+	if((sim_datapath_out != 16'd11) || sim_Z_out == 1) err = 1;
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 	//Test #3: ANDING Ain = 23045 and Bin = 19213
@@ -257,7 +257,7 @@ $display("TEST #2: Subtracting 13 and 4 right shifted by 1 (14) which outputs 10
 
 	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, 16'd23045 & 16'd19213);
 
-	if((sim_datapath_out != (16'd23045 & 16'd19213))) err = 1;
+	if((sim_datapath_out != (16'd23045 & 16'd19213)) || sim_Z_out == 1) err = 1;
 
 	//////////////////////////////////////////////////////////////////////////////////////////////////
 	//Test #4: Notting Bin: 1
@@ -316,7 +316,228 @@ $display("TEST #2: Subtracting 13 and 4 right shifted by 1 (14) which outputs 10
 
 	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, 16'b0000000001111101);
 
-	if((sim_datapath_out != 16'b0000000001111101)) err = 1;
+	if((sim_datapath_out != 16'b0000000001111101)|| sim_Z_out == 1) err = 1;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//TEST SETTING ASEL AND BSEL TO 1 to make sure it uses 16'b0 for A and 11'b0+4'b[datapath_in]
+	//Test #5: Setting ASEL to 1
+	$display("Test #5: setting ASEL to 1, should make A all zeros");
+	
+	//Write 23045 to R4
+	sim_clk = 0;
+	#2;
+    sim_write = 1'b1;
+	sim_vsel = 1;	  //mux select Dataath_in as input
+	sim_writenum = 3'b100; //Write to R4
+	sim_datapath_in = 16'd23045; //16 bit value of 23045 in dec
+	sim_clk = 1'b1;
+
+    #5;
+	sim_clk = 0;
+	#2;
+
+	//Write 19213 to R1
+	sim_writenum = 1;
+	sim_datapath_in = 16'd19213;
+	sim_clk = 1'b1;
+
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R4 (23045) to a
+	sim_write = 1'b0;
+	sim_readnum = 3'b100;
+	sim_loada = 1;
+	sim_loadb = 0;
+	sim_loadc = 0;
+	sim_asel = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R1 (19213) to b
+
+	sim_readnum = 1;
+	sim_loada = 0;
+	sim_loadb = 1;
+	sim_bsel = 0;
+	sim_shift = 2'b00;
+	sim_ALUop = 2'b00; //Add
+	sim_loads = 0;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	// Load into C, and write to R2
+
+	sim_loadb = 0;
+	sim_loadc = 1;
+	sim_vsel = 0;
+	sim_writenum = 2;
+	sim_write = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+	sim_clk = 1;
+
+	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, 16'd19213);
+
+	if((sim_datapath_out != (16'd19213)) || sim_Z_out == 1) err = 1;
+
+	///////////////////////////////////////////////////////////////////////////////////////////////
+	//Test 6: setting ASEL and BSEL to 1, should make A all zeros and b different
+	$display("Test #6: setting ASEL and BSEL to 1, should make A all zeros and b different");
+	
+	//Write 23045 to R4
+	sim_clk = 0;
+	#2;
+    sim_write = 1'b1;
+	sim_vsel = 1;	  //mux select Dataath_in as input
+	sim_writenum = 3'b100; //Write to R4
+	sim_datapath_in = 16'd23045; //16 bit value of 23045 in dec
+	sim_clk = 1'b1;
+
+    #5;
+	sim_clk = 0;
+	#2;
+
+	//Write 19213 to R1
+	sim_writenum = 1;
+	sim_datapath_in = 16'd19213;
+	sim_clk = 1'b1;
+
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R4 (23045) to a
+	sim_write = 1'b0;
+	sim_readnum = 3'b100;
+	sim_loada = 1;
+	sim_loadb = 0;
+	sim_loadc = 0;
+	sim_asel = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R1 (19213) to b
+
+	sim_readnum = 1;
+	sim_loada = 0;
+	sim_loadb = 1;
+	sim_bsel = 1;
+	sim_shift = 2'b00;
+	sim_ALUop = 2'b00; //Add
+	sim_loads = 0;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	// Load into C, and write to R2
+
+	sim_loadb = 0;
+	sim_loadc = 1;
+	sim_vsel = 0;
+	sim_asel = 1;
+	sim_writenum = 2;
+	sim_write = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+	sim_clk = 1;
+
+	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, {11'b0, sim_datapath_in[4:0]});
+
+	if((sim_datapath_out != ({11'b0, sim_datapath_in[4:0]}) || sim_Z_out == 1)) err = 1;
+
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	//TEST z_out to be 1 when output is zero
+	//Test #7: Setting ASEL to 0
+	$display("Test #5: setting ASEL to 1, should make A all zeros");
+	
+	//Write 23045 to R4
+	sim_clk = 0;
+	#2;
+    sim_write = 1'b1;
+	sim_vsel = 1;	  //mux select Dataath_in as input
+	sim_writenum = 3'b100; //Write to R4
+	sim_datapath_in = 16'd23045; //16 bit value of 23045 in dec
+	sim_clk = 1'b1;
+
+    #5;
+	sim_clk = 0;
+	#2;
+
+	//Write 0 to R1
+	sim_writenum = 1;
+	sim_datapath_in = 16'd0;
+	sim_clk = 1'b1;
+
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R4 (23045) to a
+	sim_write = 1'b0;
+	sim_readnum = 3'b100;
+	sim_loada = 1;
+	sim_loadb = 0;
+	sim_loadc = 0;
+	sim_asel = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	//Load R1 (0) to b
+
+	sim_readnum = 1;
+	sim_loada = 0;
+	sim_loadb = 1;
+	sim_bsel = 0;
+	sim_shift = 2'b00;
+	sim_ALUop = 2'b00; //Add
+	sim_loads = 0;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+
+	// Load into C, and write to R2
+
+	sim_loadb = 0;
+	sim_loadc = 1;
+	sim_vsel = 0;
+	sim_asel = 1;
+	sim_writenum = 2;
+	sim_write = 1;
+	
+	sim_clk = 1;
+	#5;
+	sim_clk = 0;
+	#2;
+	sim_clk = 1;
+
+	$display("Read datapath out, Output is %b, we expected %b", sim_datapath_out, 16'd0);
+
+	if((sim_datapath_out != (16'd0) || sim_Z_out != 1)) err = 1;
+
 
     end
 
